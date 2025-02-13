5c846eecd5ed21aa4bc76f8784525e1a7a1c3822
oricardos <oricardo.dev@gmail.com>
2025-02-13 10:51:19 -0300

    Criação de componentes no input.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        verde: { //criar
+          200: '#acef75',
+          300: '#91EE77',
+          900: '#16281F',
+          950: '#0F1C15',
+        },
+        green: { //sobrescrever
+          300: '#91EE77', //...
+        }
+      },
+      spacing: {
+        22: '5.5rem'
+      },
+      fontSize: {
+        '2xs': '.5rem',
+      }
+    },
   },
   plugins: [],
 }
