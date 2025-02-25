278a3810029eed6326742a788847c19f78211a71
oricardos <oricardo.dev@gmail.com>
2025-02-25 12:07:43 -0300

    add tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         sm: '1rem'
       }
     },
+    fontFamily: {
+      sans: ['DM Sans', "system-ui", "sans-serif"],
+      serif: ['DM Serif Text', "Georgia", "serif"]
+    },
     extend: {
       keyframes: {
         slideIn: {
